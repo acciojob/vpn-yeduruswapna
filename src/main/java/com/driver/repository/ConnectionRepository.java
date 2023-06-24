5d25e884e97f550de35710b5651553b1e3213b87
yeduruswapna <swapnayeduru@gmail.com>
2023-06-24 18:37:00 +0530

    First Commit

--- src/main/java/com/driver/repository/ConnectionRepository.java
+++ src/main/java/com/driver/repository/ConnectionRepository.java
@@ -3,6 +3,8 @@ package com.driver.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+
 @Repository
 public interface ConnectionRepository extends JpaRepository<Connection, Integer> {
 }
